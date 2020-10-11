87555e0dbd03677bfd7f2a83088fae3305d80a06
Filippo Airaldi <filippoairaldi@gmail.com>
2020-10-11 20:00:03 +0200

    made some order

--- main.cpp
+++ main.cpp
@@ -3,13 +3,11 @@
 
 int main()
 {
-    auto t1 = std::chrono::high_resolution_clock::now();
     for (int i = 0; i < 5; ++i)
     {
         uuids::uuid u = uuids::uuid::create();
-        // std::cout << u << std::endl;
+       	std::cout << u << std::endl;
     }
-    std::cout << std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::high_resolution_clock::now() - t1).count() << std::endl;
 
     std::cout << "Finished!" << std::endl;
 
